92e962ed0a3ab1ab2c985ae3f6f93538f0354ef5
ismet <ismntr@icloud.com>
2022-02-01 13:52:56 +0300

    giriş çıkış projesi

---
+++ Çalışma-02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Usem.Calisma01
+{
+    class OgrenciKayit
+    {
+        static void Main()
+        {
+            string giris;
+            string cikis;
+
+            giris = Console.ReadLine();
+            cikis = "Giriş :  " + giris;
+
+            Console.WriteLine(cikis);
+
+        }
+    }
+}
+
