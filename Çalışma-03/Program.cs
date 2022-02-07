b955bb6165eb54bf42d16c42e2c0e889b4d9f55e
ismet <ismntr@icloud.com>
2022-02-07 11:18:06 +0300

    loop-2 ve loop-3 denemesi

--- Çalışma-03/Program.cs
+++ Çalışma-03/Program.cs
@@ -6,18 +6,41 @@ namespace Calisma02
     {
         static void Main(string[] args)
         {
-            int b = 0;
 
-            for (int i = 0; true; i++)
+
+
+
+
+            int j = 2;
+            for (int i = 1; i < 100; i = i * 2)
             {
-                Console.WriteLine(i + 1);
+                j = j - 1;
+                while (j < 25)
+                {
+                    j = j + 5;
+                    Console.WriteLine(j);
+                }
             }
 
-            Console.WriteLine("Sonlandı");
+
+
+
+            int p = 2;
+            for (int q = 0; q < 32; q = q * 2)
+            {
+                while (p < q)
+                {
+                    p = p * 2;
+                    Console.WriteLine("p değeri: " + p + "\n" + "q değeri: " + q);
+                }
+                q = p - q;
+                Console.WriteLine("p değeriii: " + p + "\n" + "q değeriii: " + q);
+            }
+            Console.WriteLine("p değeriiiiiiiiii: " + p + "\n" + "q değeriiiiiiiiiiiiiiiiiiiiii: ");
+
         }
     }
 }
-
 
 // bool devam = true;
 
@@ -34,3 +57,10 @@ namespace Calisma02
 //         devam = false;
 //     }
 // }
+
+
+// Doğru döngüyü seçmek​
+// While		: yineleme sayısını tam olarak bilmiyorsan​
+// Do While	    : yineleme sayısını tam olarak bilmiyorsan ama en az bir kez işlem yapılsın istiyorsan​
+// For			: yineleme sayısını tam olarak biliyorsun​
+// For Each	    : bir koleksiyonda yer alan her bir eleman için işlem yapmak istiyorsan​
