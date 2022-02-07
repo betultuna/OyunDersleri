015340fb4e7fa444fc7953c92c91d854a35944e0
ismet <ismntr@icloud.com>
2022-02-07 13:57:47 +0300

    diziler 4 elemanlı dizi ve 3 tanesini temizleme

---
+++ Çalışma-04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Calisma02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = { 5, 7, 12, 9 };
+            string[] sDizi = { "test", "fest", "mest", "kest" };
+             foreach (int sayi in dizi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            Console.WriteLine("-------------------------------");
+
+            Array.Clear(dizi,1,3);
+
+            Console.WriteLine("-------------------------------");
+
+            foreach (int sayi in dizi)
+            {
+                Console.WriteLine(sayi);
+            }
+        }
+    }
+}
